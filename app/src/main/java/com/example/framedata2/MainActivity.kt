968575ccc9a2ca374f95eec08e14a42863052d1a
amanpsp <78340076+amanpsp@users.noreply.github.com>
2021-07-19 17:06:03 -0700

    test to see if all 15 characters are loaded into the datasource

--- app/src/main/java/com/example/framedata2/MainActivity.kt
+++ app/src/main/java/com/example/framedata2/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.framedata2
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.framedata2.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textview)
+        textView.text = Datasource().loadCharacters().size.toString()
     }
 }
