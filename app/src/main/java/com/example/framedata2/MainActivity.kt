5cb233c8d65d736fef06c1a979c4b16ea990cbe3
amanpsp <78340076+amanpsp@users.noreply.github.com>
2021-07-20 13:50:20 -0700

    successfully made all 15 characters appear in recyclerview

--- app/src/main/java/com/example/framedata2/MainActivity.kt
+++ app/src/main/java/com/example/framedata2/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.framedata2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.framedata2.adapter.ItemAdapter
+import com.example.framedata2.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val myDataset = Datasource().loadCharacters()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
 
     }
 }
