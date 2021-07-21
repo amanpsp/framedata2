0cdeab29a1c8bfc6aac9ce94c479b7c875d44214
amanpsp <78340076+amanpsp@users.noreply.github.com>
2021-07-21 15:30:48 -0700

    changed the 15 character buttons into imagebuttons

--- app/src/main/java/com/example/framedata2/data/Datasource.kt
+++ app/src/main/java/com/example/framedata2/data/Datasource.kt
@@ -7,21 +7,21 @@ class Datasource {
 
     fun loadCharacters():List<Character>{
         return listOf<Character>(
-            Character(R.string.anji),
-            Character(R.string.axl),
-            Character(R.string.chipp),
-            Character(R.string.faust),
-            Character(R.string.gio),
-            Character(R.string.ino),
-            Character(R.string.ky),
-            Character(R.string.leo),
-            Character(R.string.may),
-            Character(R.string.millia),
-            Character(R.string.nago),
-            Character(R.string.pot),
-            Character(R.string.ram),
-            Character(R.string.sol),
-            Character(R.string.zato),
+            Character(R.string.anji, R.drawable.anji),
+            Character(R.string.axl, R.drawable.axl),
+            Character(R.string.chipp, R.drawable.chipp),
+            Character(R.string.faust, R.drawable.faust),
+            Character(R.string.gio, R.drawable.gio),
+            Character(R.string.ino, R.drawable.ino),
+            Character(R.string.ky, R.drawable.ky),
+            Character(R.string.leo, R.drawable.leo),
+            Character(R.string.may, R.drawable.may),
+            Character(R.string.millia, R.drawable.millia),
+            Character(R.string.nago, R.drawable.nago),
+            Character(R.string.pot, R.drawable.pot),
+            Character(R.string.ram, R.drawable.ram),
+            Character(R.string.sol, R.drawable.sol),
+            Character(R.string.zato, R.drawable.zato),
         )
 
 
